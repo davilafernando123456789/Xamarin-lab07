37c12fe4c4a47afdc098eaca4c9b2d0eed0c70c9
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2023-10-22 00:05:09 -0500

    Agregando lista de canciones

--- Ejercicio07/Ejercicio07/CssDemo.xaml.cs
+++ Ejercicio07/Ejercicio07/CssDemo.xaml.cs
@@ -18,10 +18,22 @@ namespace Ejercicio07
 
             List<Song> songs = new List<Song>
         {
-            new Song { Title = "No se va", Artist = "Morat" },
-            new Song { Title = "Eres mi nuevo vicio", Artist = "CNCO" },
-            new Song { Title = "Shape of you", Artist = "Ed Sheeran" },
-            new Song { Title = "Remember", Artist = "Tylor Swift" },
+         new Song { Title = "No se va", Artist = "Morat" },
+        new Song { Title = "Eres mi nuevo vicio", Artist = "CNCO" },
+        new Song { Title = "Shape of you", Artist = "Ed Sheeran" },
+        new Song { Title = "Remember", Artist = "Taylor Swift" },
+        new Song { Title = "Bohemian Rhapsody", Artist = "Queen" },
+        new Song { Title = "Billie Jean", Artist = "Michael Jackson" },
+        new Song { Title = "Hotel California", Artist = "Eagles" },
+        new Song { Title = "Imagine", Artist = "John Lennon" },
+        new Song { Title = "Uptown Funk", Artist = "Mark Ronson ft. Bruno Mars" },
+        new Song { Title = "Thriller", Artist = "Michael Jackson" },
+        new Song { Title = "Despacito", Artist = "Luis Fonsi ft. Daddy Yankee" },
+        new Song { Title = "Sweet Child o' Mine", Artist = "Guns N' Roses" },
+        new Song { Title = "Yesterday", Artist = "The Beatles" },
+        new Song { Title = "Stairway to Heaven", Artist = "Led Zeppelin" },
+        new Song { Title = "Wonderwall", Artist = "Oasis" }
+
         };
 
             songListView.ItemsSource = songs;
